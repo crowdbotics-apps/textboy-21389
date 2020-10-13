7f9513b2a23a6e9c273c020da2e3dcb2636e6d55
Crowdbotics <team@crowdbotics.com>
2020-10-13 20:17:46 +0000

    Update store

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,6 +3,7 @@ export const signup_post_api_v1_signup_create = data => ({
   type: types.SIGNUP_POST_API_V1_SIGNUP_CREATE,
   data
 })
+
 export const signup_post_api_v1_signup_createSucceeded = (
   response,
   starter
@@ -11,6 +12,7 @@ export const signup_post_api_v1_signup_createSucceeded = (
   response,
   starter
 })
+
 export const signup_post_api_v1_signup_createFailed = (error, starter) => ({
   type: types.SIGNUP_POST_API_V1_SIGNUP_CREATE_FAILED,
   error,
