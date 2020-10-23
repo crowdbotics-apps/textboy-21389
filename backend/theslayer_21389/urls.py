eff7105da5411736f3c1768c9f59cd3d034ad59f
Crowdbotics <team@crowdbotics.com>
2020-10-23 02:23:53 +0000

    Update admin title

--- backend/theslayer_21389/urls.py
+++ backend/theslayer_21389/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "textboy"
-admin.site.site_title = "textboy Admin Portal"
-admin.site.index_title = "textboy Admin"
+admin.site.site_header = "textboy1"
+admin.site.site_title = "textboy1 Admin Portal"
+admin.site.index_title = "textboy1 Admin"
 
 # swagger
 api_info = openapi.Info(
